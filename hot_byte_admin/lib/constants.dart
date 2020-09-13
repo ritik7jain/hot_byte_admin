3f32ab89efdf3936f0b685249dcdb91568d63569
anushka <anushkamaheshwari02@gmail.com>
2020-09-14 01:56:29 +0530

    commited

--- hot_byte_admin/lib/constants.dart
+++ hot_byte_admin/lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const TextInputDecoration = InputDecoration(
-                   fillColor: Colors.blue,
+                   fillColor: Colors.white,
                    filled: true,
                    focusedBorder: OutlineInputBorder(
                      borderSide: BorderSide(color: Colors.yellow, width: 2.0),
