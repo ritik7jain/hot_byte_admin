096b3384556dced22bee4411fa31b70d078ddb5a
Ritik Jain <jritik324@gmail.com>
2020-09-14 02:09:03 +0530

    register

--- hot_byte_admin/lib/register.dart
+++ hot_byte_admin/lib/register.dart
@@ -11,114 +11,133 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
-
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
-  String email= '';
-  String password='';
-  String error='';
+  String email = '';
+  String password = '';
+  String error = '';
   bool loading = false;
-    @override
+  @override
   Widget build(BuildContext context) {
-     return loading ? Loading() : Scaffold(
-
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.yellow[700],
-        elevation: 0.0,
-        title: Text("Sign up to Hot Byte",
-        style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-            fontSize: 23
-          ),),
-        actions: <Widget>[
-          FlatButton.icon(
-            icon: Icon(Icons.person),
-            label: Text("Sign In",
-             style: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-            fontSize: 20
-          ),),
-            onPressed: () async{
-              widget.toggleView();
-
-            },
-
-          ),
-        ],
-      ),
-      body: Container(
-         decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/hot_byte.jpg"),
-              // fit: BoxFit.cover,
-            )
-          ),
-        padding: EdgeInsets.symmetric(vertical :20, horizontal:50),
-        child:Form(
-          key:_formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              TextFormField(
-                validator: (val) => val.isEmpty ? ' Enter an email' : null,
-                 decoration: TextInputDecoration.copyWith(hintText:"Email"),
-                onChanged: (val){
-                  setState(() => email = val);},
-              ),
-              SizedBox(height: 20.0,),
-              TextFormField(
-                validator: (val) => val.length<6 ? ' Enter a password longer than 6 character' : null,
-                decoration: TextInputDecoration.copyWith(hintText:"Password"),
-                obscureText: true,
-                onChanged: (val){
-                   setState(() => password  = val);
-                },
-              ),
-              SizedBox(height: 20.0,),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 0, 200, 0),
-                child: RaisedButton(
-                  color: Colors.yellow[700],
-                  child: Text(
-                    "Register",
-                     style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                      ),
+    return loading
+        ? Loading()
+        : SafeArea(
+                  child: Scaffold(
+              body: Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(color: Colors.white),
                   ),
-                  onPressed: () async{
-                    if(_formKey.currentState.validate()){
-                        setState(()=>loading = true);
-                      dynamic result = await _auth.registerWithEmailPassword(email,password);
-                      if(result == null){
-                        setState((){
-                          error="Please enter correct email";
-                          loading=false;
-                         } );
-                      }
-                    }
-                    
-
-                  },
-                ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Expanded(
+                        flex: 2,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            SizedBox(
+                              height: 80,
+                            ),
+                            Container(
+                              height: MediaQuery.of(context).size.height * .50,
+                              width: 300,
+                              child: Form(
+                                key: _formKey,
+                                child: Column(
+                                  children: <Widget>[
+                                    SizedBox(height: 20.0),
+                                    TextFormField(
+                                      validator: (val) =>
+                                          val.isEmpty ? ' Enter an email' : null,
+                                      decoration: TextInputDecoration.copyWith(
+                                          hintText: "Email"),
+                                      onChanged: (val) {
+                                        setState(() => email = val);
+                                      },
+                                    ),
+                                    SizedBox(
+                                      height: 20.0,
+                                    ),
+                                    TextFormField(
+                                      validator: (val) => val.length < 6
+                                          ? ' Enter a password longer than 6 character'
+                                          : null,
+                                      decoration: TextInputDecoration.copyWith(
+                                          hintText: "Password"),
+                                      obscureText: true,
+                                      onChanged: (val) {
+                                        setState(() => password = val);
+                                      },
+                                    ),
+                                    SizedBox(
+                                      height: 20.0,
+                                    ),
+                                    Padding(
+                                      padding:
+                                          const EdgeInsets.fromLTRB(0, 0, 200, 0),
+                                      child: RaisedButton(
+                                        color: Colors.yellow[700],
+                                        child: Text(
+                                          "Register",
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 18,
+                                          ),
+                                        ),
+                                        onPressed: () async {
+                                          if (_formKey.currentState.validate()) {
+                                            setState(() => loading = true);
+                                            dynamic result = await _auth
+                                                .registerWithEmailPassword(
+                                                    email, password);
+                                            if (result == null) {
+                                              setState(() {
+                                                error =
+                                                    "Please enter correct email";
+                                                loading = false;
+                                              });
+                                            }
+                                          }
+                                        },
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 20,
+                                    ),
+                                    Text(
+                                      error,
+                                      style: TextStyle(
+                                          color: Colors.red, fontSize: 14.0),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                            //  Expanded(
+                            //   child: CircleAvatar(
+                            //     radius: 150,
+                            //     backgroundImage:
+                            //         AssetImage("assets/hot_byte.jpg"),
+                            //     backgroundColor: Colors.white,
+                                
+                            //   ),
+                            // ),
+                            Expanded(
+                                                          child: Image(
+                                image: AssetImage("assets/hot_byte.jpg"),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
               ),
-              SizedBox(height: 20,),
-              Text(
-                error,
-                style: TextStyle(
-                  color: Colors.red,
-                  fontSize: 14.0
-                ),
-              )
-
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+        );
   }
 }
